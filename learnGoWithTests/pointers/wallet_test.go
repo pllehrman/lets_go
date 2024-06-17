4a42c707ac2ffa12e19aff4b752dfcd79cdf06f5
Peter Lehrman <peter.lehrman@liveramp.com>
2024-06-17 13:14:42 -0700

    finished a couple more modules.

--- learnGoWithTests/pointers/wallet_test.go
+++ learnGoWithTests/pointers/wallet_test.go
@@ -14,7 +14,9 @@ func TestWallet(t *testing.T) {
 
 	t.Run("withdraw", func (t *testing.T) {
 		wallet := Wallet {balance: Bitcoin(20)}
-		wallet.Withdraw(Bitcoin(5))
+		err := wallet.Withdraw(Bitcoin(5))
+
+		assertNoError(t, err)
 		assertBalance(t, wallet, Bitcoin(15))
 	})
 
@@ -36,6 +38,13 @@ func assertBalance(t testing.TB, wallet Wallet, want Bitcoin) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one.")
 	}
 }
 
