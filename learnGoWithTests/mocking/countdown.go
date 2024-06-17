a58b450ab926e33fc3c23295cd18da1eef8a2ca2
Peter Lehrman <peter.lehrman@liveramp.com>
2024-06-17 16:05:44 -0700

    onto concurrency

---
+++ learnGoWithTests/mocking/countdown.go
@@ -0,0 +1,66 @@
+// "Spying in" refers to creating a struct that implements a similar named method under the same interface but doesn't perform all the exhaustive functional actions.
+// For example, the sleeper interface implements the Sleep() method which can be used by SpyCountdownOperations to count the number of method calls. It doesn't
+// "sleep" in the sense that it times the function out.
+
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"time"
+)
+
+const finalWord = "Go!"
+const countdownStart = 3
+
+type Sleeper interface {
+	Sleep()
+}
+
+type SpyCountdownOperations struct {
+	Calls []string
+}
+
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+const write = "write"
+const sleep = "sleep"
+
+
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep func(time.Duration)
+}
+
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+
+type SpyTime struct {
+	durationSlept time.Duration
+}
+
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+func Countdown(out io.Writer, sleeper Sleeper){
+	for i := countdownStart; i > 0; i-- {
+		fmt.Fprintln(out, i)
+		sleeper.Sleep()
+	}
+	fmt.Fprint(out, finalWord)
+}
+
+func main() {
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
+	Countdown(os.Stdout, sleeper)
+}
